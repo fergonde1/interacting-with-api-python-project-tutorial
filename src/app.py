729ff7ef9b972d73cab7e6eee0c93fadac5bc5e0
fergonde1 <157165119+fergonde1@users.noreply.github.com>
2024-03-30 20:11:54 +0000

    mi perfil

--- src/app.py
+++ src/app.py
@@ -1,15 +1,35 @@
 import os
-from sqlalchemy import create_engine
+import spotipy
 import pandas as pd
+import matplotlib.pyplot as plt
 from dotenv import load_dotenv
-
-# load the .env file variables
+from spotipy.oauth2 import SpotifyClientCredentials
 load_dotenv()
 
-# 1) Connect to the database here using the SQLAlchemy's create_engine function
+client_id = os.environ.get('CLIENT_ID')
+client_secret = os.environ.get('CLIENT_SECRET')
+sp = spotipy.Spotify(auth_manager = SpotifyClientCredentials(client_id = client_id,
+                                                              client_secret = client_secret))
+artist_id = "3TVXtAsR1Inumwj472S9r4"
 
-# 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
+response = sp.artist_top_tracks("0DCw6lHkzh9t7f8Hb4Z0Sx")
+if response:
+    canciones = response["tracks"]
+    listacanciones = []
+    for cancion in canciones:
+        cancion2 = {}
+        cancion2["name"] = cancion["name"]
+        cancion2["popularity"] = cancion["popularity"]
+        cancion2["duration_minutes"] = (cancion["duration_ms"] / (1000 * 60)) % 60
+        listacanciones.append(cancion2)
 
-# 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
+cancionesdf = pd.DataFrame.from_records(listacanciones)
+cancionesdf.sort_values(["popularity"], inplace = True)
 
-# 4) Use pandas to print one of the tables as dataframes using read_sql function
+print(cancionesdf.head(3))
+
+plt.scatter(cancionesdf["duration_minutes"], cancionesdf["popularity"], alpha=0.5)
+plt.xlabel("Duración (minutos)")
+plt.ylabel("Popularidad")
+plt.title("Duración vs Popularidad de las Canciones Principales")
+plt.show()
